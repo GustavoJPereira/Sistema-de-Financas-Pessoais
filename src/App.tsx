6d0a069ff56401763db1b7bb26bb52b4d84ddd74
Escravo <gustavojosepereira2021@gmail.com>
2021-10-07 10:43:00 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,13 @@ const App = () => {
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-        ...
+        
+        {/* Area de informações */}
+
+        {/* Area de inserção */}
+
+        {/* Area de itens */}
+
       </C.Body>
     </C.Container>
   );
